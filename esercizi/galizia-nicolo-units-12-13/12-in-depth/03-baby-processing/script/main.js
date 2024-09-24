a75360382239c6420e0c55441710034e452e72ef
nicogalii <galizianicolo2004@gmail.com>
2024-09-24 20:20:02 +0200

    C:/Program Files/Git/add

---
+++ esercizi/galizia-nicolo-units-12-13/12-in-depth/03-baby-processing/script/main.js
@@ -0,0 +1,44 @@
+/**
+ * @file        main.js
+ * @author      Nicolò Galizia
+ * @date        2024-09-24
+ * @description 
+ */
+
+// Add babies in array
+babies.push({
+    name: "Luca",
+    months: 5,
+    noises: ["sium", "gaa"],
+    favoriteFoods: ["latte", "banana"],
+    outfit = {
+
+    }
+}, {
+    name: "Matilde",
+    months: 10,
+    noises: ["uee", "goo"],
+    favoriteFoods: ["pane", "cioccolato"]
+});
+
+babies.unshift({
+    name: "Paolo",
+    months: 3,
+    noises: ["goo", "sium"],
+    favoriteFoods: ["cereali", "latte"]
+}, {
+    name: "Noemi",
+    months: 5,
+    noises: ["blee", "shh"],
+    favoriteFoods: ["salame", "pastina"]
+});
+
+/**
+ * Descrizione della funzione
+ * @param {TipoInput1} NomeInput1 - DescrizioneInput1
+ * @param {TipoInput2} NomeInput2 - DescrizioneInput2
+ * @returns {TipoOutput} - DescrizioneOutput
+ */
+function getBabyOutfit() {
+
+}
---
+++ esercizi/galizia-nicolo-units-12-13/13-object-oriented/01-dogspeak/script/main.js
@@ -0,0 +1,13 @@
+/**
+ * @file        main.js
+ * @author      Nicolò Galizia
+ * @date        2024-09-24
+ * @description 
+ */
+
+/**
+ * Descrizione della funzione
+ * @param {TipoInput1} NomeInput1 - DescrizioneInput1
+ * @param {TipoInput2} NomeInput2 - DescrizioneInput2
+ * @returns {TipoOutput} - DescrizioneOutput
+ */
